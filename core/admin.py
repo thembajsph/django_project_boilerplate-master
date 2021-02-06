55bd34ac67f95192c31fcda0b86a051da5a2ab3d
thembajsph <thembajsph1@gmail.com>
2021-02-06 03:13:08 +0200

    initial commit

---
+++ core/admin.py
@@ -0,0 +1,81 @@
+from django.contrib import admin
+
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile
+
+
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+
+
+make_refund_accepted.short_description = 'Update orders to refund granted'
+
+
+def make_being_delivered(modeladmin, request, queryset):
+    queryset.update(received=False, being_delivered=True)
+
+
+make_being_delivered.short_description = 'Update orders to delivery pending'
+
+
+def make_delivery_received(modeladmin, request, queryset):
+    queryset.update(being_delivered=False, received=True)
+
+
+make_delivery_received.short_description = 'Update orders to delivery received'
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'ordered',
+                    'being_delivered',
+                    'received',
+                    'refund_requested',
+                    'refund_granted',
+                    'shipping_address',
+                    'billing_address',
+                    'payment',
+                    'coupon'
+                    ]
+    list_display_links = [
+        'user',
+        'shipping_address',
+        'billing_address',
+        'payment',
+        'coupon'
+    ]
+    list_filter = ['ordered',
+                   'being_delivered',
+                   'received',
+                   'refund_requested',
+                   'refund_granted']
+    search_fields = [
+        'user__username',
+        'ref_code'
+    ]
+    actions = [make_refund_accepted, make_being_delivered, make_delivery_received]
+    # actions = [make_being_delivered]
+    # actions = [make_delivery_received]
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'street_address',
+        'apartment_address',
+        'country',
+        'zip',
+        'address_type',
+        'default'
+    ]
+    list_filter = ['default', 'address_type', 'country']
+    search_fields = ['user', 'street_address', 'apartment_address', 'zip']
+
+
+admin.site.register(Item)
+admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Payment)
+admin.site.register(Coupon)
+admin.site.register(Refund)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(UserProfile)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/thembajsph/Downloads/try/django_project_boilerplate-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
