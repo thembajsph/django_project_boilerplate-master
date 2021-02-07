779a5b02e33630549bc96e947a34306da7643eb5
thembajsph <thembajsph1@gmail.com>
2021-02-08 01:26:08 +0200

    changed to production settings

--- tjecommerce/wsgi.py
+++ tjecommerce/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tjecommerce.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tjecommerce.settings.production')
 
 application = get_wsgi_application()
